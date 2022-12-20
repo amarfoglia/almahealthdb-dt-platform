3fde3a284b54f35266006e9730304240dfd6260a
Filippo Nardini <filippo.nardini@studio.unibo.it>
2022-12-20 11:47:28 +0100

    build: fix wrong zio-streams dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
       val `zio-http` = "dev.zio" %% "zio-http" % "0.0.3"
       val `zio-json` = "dev.zio" %% "zio-json" % "0.3.0"
       val `zio-kafka` = "dev.zio" %% "zio-kafka" % "2.0.1"
-      val `zio-streams` = "dev.zio" %% "zio" % zioVersion
+      val `zio-streams` = "dev.zio" %% "zio-streams" % zioVersion
       val `zio-test` = "dev.zio" %% "zio-test" % zioVersion
       val `zio-test-junit` = "dev.zio" %% "zio-test-junit" % zioVersion
     }
